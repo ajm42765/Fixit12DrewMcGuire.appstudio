fecbece89a581d6445b5e68e1f2c612b1d58a09a
ajm42765 <60629760+ajm42765@users.noreply.github.com>
2020-04-14 21:43:17 -0500

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,15 @@
+var request = new XMLHttpRequest()
+
+request.open('Get', 'https://cloud.google.com/maps-platform/places/?utm_source=google&utm_medium=cpc&utm_campaign=FY18-Q2-global-demandgen-paidsearchonnetworkhouseads-cs-maps_contactsal_saf&utm_content=text-ad-none-none-DEV_c-CRE_315916117565-ADGP_Hybrid+%7C+AW+SEM+%7C+BKWS+~+Google+Maps+Places+API-KWID_43700039136946099-aud-581578347266:kwd-22859391737-userloc_9019689&utm_term=KW_google%20places%20api-ST_google+places+api&gclid=CjwKCAjwvtX0BRAFEiwAGWJyZPRYtEZnm5lYxco5HSNy-Jy4mrFKyI3UfvGPoQI47VeQRsNpfoQA1hoCM8cQAvD_BwE', true)
+
+request.onload = function() {
+}
+
+request.send
+Textarea1.onclick=function(){
+  (name, address, ranking)
+}
+
+Button1.onclick=function(){
+  alert(Textarea1)
+}
